c1877e91a2b7231543445b54797850709c457579
indextrown <indextrown@gmail.com>
2025-12-09 15:04:38 +0900

    docs: 문서 호스팅

--- Package.swift
+++ Package.swift
@@ -5,12 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "BoxOffices",
-    platforms: [.iOS(.v17)],
+    platforms: [.iOS(.v17), .macOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BoxOffices",
             targets: ["BoxOffices"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
